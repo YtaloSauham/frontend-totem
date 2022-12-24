d817370901958985f111b1e86da50bbe3e89d636
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2022-12-24 20:38:17 -0300

    first commit 

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.tsx", 
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./index.html",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
