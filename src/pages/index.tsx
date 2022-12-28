06a07da31fef6daecc1d43ae0dd6dee620a06767
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-12-28 15:44:09 -0300

    add inputs navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -158,6 +158,36 @@ export default function Home() {
               </span>
             </div>
           </li>
+
+          <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <div className="flex items-center pl-3">
+              <input
+                type="radio"
+                value="SHOPPING CENTER"
+                name="navigation"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                onClick={(e) => setSentido(e.target.value)}
+              />
+              <span className="py-3 ml-2 w-full text-sm font-medium text-white">
+                Shopping Center
+              </span>
+            </div>
+          </li>
+
+          <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <div className="flex items-center pl-3">
+              <input
+                type="radio"
+                value="PRAIA"
+                name="navigation"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                onClick={(e) => setSentido(e.target.value)}
+              />
+              <span className="py-3 ml-2 w-full text-sm font-medium text-white">
+                Praia
+              </span>
+            </div>
+          </li>
         </ul>
       </div>
 
